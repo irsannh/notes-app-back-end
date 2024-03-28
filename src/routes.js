5bfbd0dd13483c4c67af6f470a93e00a9513d488
irsannh <irsannur.h@gmail.com>
2024-03-28 11:55:18 +0700

    Fix Another Bugs

--- src/routes.js
+++ src/routes.js
@@ -1,35 +1,35 @@
 const {
-  addNoteHandler,
   getAllNotesHandler,
-  getNoteByIdHandler,
+  getNotesByIdHandler,
+  addNotesHandler,
   editNoteByIdHandler,
   deleteNoteByIdHandler,
-} = require('./handler');
+} = require("./handler");
 
 const routes = [
   {
-    method: 'POST',
-    path: '/notes',
-    handler: addNoteHandler,
-  },
-  {
-    method: 'GET',
-    path: '/notes',
+    method: "GET",
+    path: "/notes",
     handler: getAllNotesHandler,
   },
   {
-    method: 'GET',
-    path: '/notes/{id}',
-    handler: getNoteByIdHandler,
+    method: "GET",
+    path: "/notes/{id}",
+    handler: getNotesByIdHandler,
   },
   {
-    method: 'PUT',
-    path: '/notes/{id}',
+    method: "POST",
+    path: "/notes",
+    handler: addNotesHandler,
+  },
+  {
+    method: "PUT",
+    path: "/notes/{id}",
     handler: editNoteByIdHandler,
   },
   {
-    method: 'DELETE',
-    path: 'notes/{id}',
+    method: "DELETE",
+    path: "/notes/{id}",
     handler: deleteNoteByIdHandler,
   },
 ];
